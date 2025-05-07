55775e12780a7bbc911e34fb26ab272b7885698b
Salehahmedeus <97753281+Salehahmedeus@users.noreply.github.com>
2025-05-07 21:58:06 +0200

    models and migration withou relations

---
+++ database/migrations/2025_05_07_194754_payments.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->enum('payment_method', ['cash', 'bank digital services', 'insurance']);
+            $table->enum('status', ['paid', 'partial', 'unpaid'])->default('unpaid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
